7699db2163cf91fe8b91ce0c6ee5c247b13d9046
Dishant <Dishantyadav19@gmail.com>
2018-02-11 20:44:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dishant/loginscreen/Exit.java
@@ -0,0 +1,17 @@
+package com.example.dishant.loginscreen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dishant on 2/11/2018.
+ */
+
+public class Exit extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exit);
+    }
+}
---
+++ app/src/main/java/com/example/dishant/loginscreen/Signup.java
@@ -0,0 +1,18 @@
+package com.example.dishant.loginscreen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dishant on 2/11/2018.
+ */
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+}
